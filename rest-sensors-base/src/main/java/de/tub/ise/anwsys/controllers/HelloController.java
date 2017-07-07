c9990995509e3648e4985fdc256b358dd8474d75
georg2014 <g.stahn@campus.tu-berlin.de>
2017-07-07 16:41:26 +0200

    Change HelloController to JonasBranch

--- rest-sensors-base/src/main/java/de/tub/ise/anwsys/controllers/HelloController.java
+++ rest-sensors-base/src/main/java/de/tub/ise/anwsys/controllers/HelloController.java
@@ -3,6 +3,8 @@ package de.tub.ise.anwsys.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +30,33 @@ public class HelloController {
 			return String.format("Welcome new User %s!", u.getName());
 		}
 	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<User> getAllUsers(){
+		return (List<User>) repository.findAll(); 
+	}
+	@RequestMapping(method=RequestMethod.GET, value="/{name}")
+	public List<User> getUser(@PathVariable String name){
+		return repository.findByName(name);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public User createUser(@RequestBody User u){
+		User user = new User(u.getName());
+		return repository.save(user);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/{name}")
+	public User updateUser(@RequestBody User u, @PathVariable String name){
+		List<User> user = repository.findByName(name);
+		((User) user).setName(u.getName());
+		return (User) repository.save(user);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/{name}")
+	public void deleteUser(@PathVariable String name){
+		repository.delete(name);
+	}
+	
 }
