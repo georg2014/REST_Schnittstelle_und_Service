8a8763bf0b7ac690c90a1c978d802975e4062f76
SirFailaL0t <jakob-feldmann@outlook.com>
2017-07-09 11:47:58 +0200

    ?

---
+++ anwsys-rest-client/src/main/java/de/tub/ise/anwsys/Server.java
@@ -0,0 +1,17 @@
+package de.tub.ise.anwsys;
+
+import javax.persistence.Entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("de.tub.ise.anwsys")
+public class Server {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+
+}
