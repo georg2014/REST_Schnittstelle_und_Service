ad812cd111f0ceb6c52f4434e2e40f8d8adbd7b3
georg2014 <g.stahn@campus.tu-berlin.de>
2017-07-09 12:07:02 +0200

    Hier mit ein paar SM's zum sehen ;)

--- rest-sensors-base/src/main/java/de/tub/ise/anwsys/Server.java
+++ rest-sensors-base/src/main/java/de/tub/ise/anwsys/Server.java
@@ -1,8 +1,15 @@
 package de.tub.ise.anwsys;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import de.tub.ise.anwsys.models.SmartMeter;
+import de.tub.ise.anwsys.repos.SmartMeterRepository;
 
 @SpringBootApplication
 @ComponentScan("de.tub.ise.anwsys")
@@ -11,5 +18,16 @@ public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
     }
-
+    
+    /*********************************FOR TESTING:START*******************************/
+    @Bean
+	CommandLineRunner init(SmartMeterRepository accountRepository) {
+		return (evt) -> Arrays.asList(
+				"SM_jhoeller,SM_dsyer,SM_pwebb,SM_ogierke,SM_rwinch,SM_mfisher,SM_mpollack,SM_jlong".split(","))
+				.forEach(
+						a -> {
+							SmartMeter sm = accountRepository.save(new SmartMeter(a));
+						});
+	}
+    /*********************************FOR TESTING:END*********************************/
 }
