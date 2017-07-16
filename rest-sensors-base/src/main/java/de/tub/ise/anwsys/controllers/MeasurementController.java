b7fddb7f31450dade75580566b0ffc1836dd3f0e
Leon <Leon@cagebot.de>
2017-07-16 14:55:09 +0200

    removed ununsed imports

--- rest-sensors-base/src/main/java/de/tub/ise/anwsys/controllers/MeasurementController.java
+++ rest-sensors-base/src/main/java/de/tub/ise/anwsys/controllers/MeasurementController.java
@@ -1,7 +1,5 @@
 package de.tub.ise.anwsys.controllers;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.tub.ise.anwsys.models.Measurement;
